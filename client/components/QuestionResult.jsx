e8614b1f97c5b2580f417670618aaaac2cc7ea64
zoeIML <zoe.lisk@gmail.com>
2018-04-20 10:27:43 +1200

    added components

---
+++ client/components/QuestionResult.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuestionResult = () => {
+  return (
+    <div className="question-result-wrapper">
+      <h1>Question Result</h1>
+    </div>
+  )
+}
+
+export default QuestionResult
