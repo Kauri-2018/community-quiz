23e9ea8605a53b5c9f35f3f8394e4689e89fa216
cameronprobert <c.probert@hotmail.co.nz>
2018-04-20 11:42:46 +1200

    added QuestionResult.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Home = () => {
+import {Link} from 'react-router-dom'
+
+const Home = (props) => {
   return (
     <div className="home-wrapper">
-      <h1>Home</h1>
+      <h1>Community Quiz</h1>
+      <div className='name-wrapper'>
+        Enter your name:
+        <input onChange={props.changeName} />
+      </div>
+      <div className='start-game-button-wrapper'>
+        <Link to='/question/play'><div className='start-game-button'>
+          Start Game
+        </div></Link>
+      </div>
     </div>
   )
 }
--- client/components/QuestionResult.jsx
+++ client/components/QuestionResult.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const QuestionResult = () => {
-  return (
-    <div className="question-result-wrapper">
-      <h1>Question Result</h1>
-    </div>
-  )
+class QuestionResult extends React.Component {
+  componentDidMount () {
+    props.submitAnswer()
+  }
+
+  render () {
+    const isCorrect = props.isCorrect
+    return (
+      <div className="question-result-wrapper">
+        <h3>Result:</h3>
+        <p className={`question ${isCorrect
+          ? 'correct'
+          : 'incorrect'}`}>
+          {isCorrect
+            ? 'Yes! Correct!'
+            : 'Sorry, that is incorrect.'}
+        </p>
+        <div className='question-result-continue-wrapper'>
+          <Link to={`/${isCorrect
+            ? 'question/make'
+            : ''}`}>
+            <div className='question-result-continue div-button'>
+              {isCorrect
+                ? 'Continue'
+                : 'Restart'}
+            </div>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
+const QuestionResult = props => 
 
 export default QuestionResult
