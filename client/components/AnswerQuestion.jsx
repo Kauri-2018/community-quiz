c2b9273d5f03fe448afefa84c1f00194e85667cd
cameronprobert <c.probert@hotmail.co.nz>
2018-04-20 11:45:21 +1200

    fixed indentation and trailing spaces

--- client/components/AnswerQuestion.jsx
+++ client/components/AnswerQuestion.jsx
@@ -11,7 +11,7 @@ class AnswerQuestion extends React.Component {
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.props.getQuestion()
       .then(question => {
         this.setState({
@@ -25,17 +25,16 @@ class AnswerQuestion extends React.Component {
       <div className="answer-question-wrapper">
         <h1>Question</h1>
         <p>{this.state.question}</p>
-        <button className="true-btn" 
-        onClick={this.props.setAnswer}
+        <button className="true-btn"
+          onClick={this.props.setAnswer}
         >True</button>
-        <button className="false-btn" 
-        onClick={this.props.setAnswer}
+        <button className="false-btn"
+          onClick={this.props.setAnswer}
         >False</button>
         <Link to="/question/play/result" className="check-answer-btn">Check answer</Link>
-    </div>
+      </div>
     )
   }
-
 }
 
 export default AnswerQuestion
