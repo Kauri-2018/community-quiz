0542cc4c169efb1c200682010255e7e93a67fbdb
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-04-20 15:22:31 +1200

    db complete

--- server/routes/apiServer.js
+++ server/routes/apiServer.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const db = require('../db')
+const db = require('../db/db.js')
 
 const router = express.Router()
 
@@ -27,14 +27,19 @@ router.get('/oldest', (req, res) => {
 })
 
 router.put('/:id/update', (req, res) => {
-  db.updateQuestion()
+  const id = Number(req.params.id)
+  const userAnswer = Number(req.query.answer)
+  db.updateQuestion(id, userAnswer)
     .then(isCorrect => {
       res.send(isCorrect)
     })
 })
 
 router.post('/add', (req, res) => {
-  db.addQuestion()
+  const name = 'Cam'
+  const question = 'What is my name'
+  const answer = true
+  db.addQuestion(question, answer, name)
     .then(question => {
       res.json({question})
     })
